ac03ed998abd0484fa6f4768fbf7b7a3f9927e02
Jamal HALAQ <jamal@Jamals-MacBook-Air.local>
2022-04-17 16:55:11 +0200

    install react router and add a new font

--- src/components/categories/categories.component.jsx
+++ src/components/categories/categories.component.jsx
@@ -2,34 +2,7 @@ import React from "react";
 import "./categories.styles.scss";
 import CategoryItem from "../category-item/category-item.component";
 
-const Categories = () => {
-  const categories = [
-    {
-      id: 1,
-      title: "hats",
-      imageUrl: "https://i.ibb.co/cvpntL1/hats.png",
-    },
-    {
-      id: 2,
-      title: "jackets",
-      imageUrl: "https://i.ibb.co/px2tCc3/jackets.png",
-    },
-    {
-      id: 3,
-      title: "sneakers",
-      imageUrl: "https://i.ibb.co/0jqHpnp/sneakers.png",
-    },
-    {
-      id: 4,
-      title: "women",
-      imageUrl: "https://i.ibb.co/GCCdy8t/womens.png",
-    },
-    {
-      id: 5,
-      title: "men",
-      imageUrl: "https://i.ibb.co/R70vBrQ/men.png",
-    },
-  ];
+const Categories = ({ categories }) => {
   return (
     <div className="categories-container">
       {categories.map((category) => (
